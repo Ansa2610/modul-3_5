efa53284135cf1c449fba07811a2335b63221146
Ansa2610 <asavilova@gmail.com>
2024-07-19 13:41:22 +0300

    Add files via upload
    edited to fulfill the condition to multiply an intergral starting with 000

--- modul 3_5.py
+++ modul 3_5.py
@@ -1,12 +1,17 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-
     first = int(str_number[0])
-    if len(str_number) > 1:
+    if first == 0:
+        first = 1
+
+    elif len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
 
-result = get_multiplied_digits('00123')
+result = get_multiplied_digits(int(str('00123')))
 print(result)
+
+
+#  При преобразовании строки(str) в число(int) первые нули убираются. int('00123') -> 123.
